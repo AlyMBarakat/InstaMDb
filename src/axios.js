b02290ffdb79e72cd296ee8e368bd8d94c346e72
AlyMBarakat <aly.m.barakat@gmail.com>
2021-05-27 19:52:08 +0200

    Update baseURL

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 
 const axios = Axios.create({
-    baseURL: 'http://api.themoviedb.org/3',
+    baseURL: 'http://api.themoviedb.org/3/',
     timeout: 10000,
     params: {
         api_key: "acea91d2bff1c53e6604e4985b6989e2",
