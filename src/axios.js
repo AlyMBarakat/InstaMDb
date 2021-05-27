cde2b35b39c575a5d63ad30ae7b09e9d18d165df
AlyMBarakat <aly.m.barakat@gmail.com>
2021-05-27 23:06:38 +0200

    Change baseURL to https

--- src/axios.js
+++ src/axios.js
@@ -2,12 +2,40 @@ import Axios from 'axios';
 
 
 const axios = Axios.create({
-    baseURL: 'http://api.themoviedb.org/3/',
+    baseURL: 'https://api.themoviedb.org/3/',
     timeout: 10000,
     params: {
         api_key: "acea91d2bff1c53e6604e4985b6989e2",
     },
 });
+
+/*
+  DEBUGGING ONLY
+*/
+
+// axios.interceptors.request.use(
+//     (req) => {
+//         console.log("Request >");
+//         console.log(req);
+//         return req;
+//     },
+//     (error) => {
+//         console.log(error);
+//         return Promise.reject(error);
+//     }
+// );
+
+// axios.interceptors.response.use(
+//     (res) => {
+//         console.log("Response >");
+//         console.log(res);
+//         return res;
+//     },
+//     (error) => {
+//         console.log(error);
+//         return Promise.reject(error);
+//     }
+// );
 
 
 export default axios;
