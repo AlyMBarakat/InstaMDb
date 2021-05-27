151303b9e927a2e86ba3fba5fe4bdd02e3e40d2b
AlyMBarakat <aly.m.barakat@gmail.com>
2021-05-27 23:11:41 +0200

    Use getMovies to test single page

--- src/features/Movies/Movies.js
+++ src/features/Movies/Movies.js
@@ -5,42 +5,43 @@
  *
  * 
  */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import {
-    ScrollView,
+    FlatList,
 } from 'react-native';
 import MovieCard from '../../shared/components/MovieCard';
+import getMovies from './API/getMovies';
 
 
-const tempMov1 = {
-    title: "Without Remorse",
-    description: "Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted.",
-    date: "2021/05/05",
-    posterURI: "https://image.tmdb.org/t/p/w185/rEm96ib0sPiZBADNKBHKBv5bve9.jpg"
+const Movies = () => {
+    const [moviesData, setMoviesData] = useState([]);
+
+    const handleMoviesData = async () => {
+        try {
+            const finalData = await getMovies(1);
+            setMoviesData(finalData);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(async () => {
+        handleMoviesData();
+    }, []);
+
+    return (
+        <FlatList
+            style={{ paddingHorizontal: 20, paddingVertical: 22, backgroundColor: '#290F0F' }}
+            data={moviesData}
+            renderItem={({ item }) => <MovieCard movieDetails={item} style={{ marginBottom: 10 }} />}
+            keyExtractor={item => item.id}
+
+        />
+    );
+
 }
-const tempMov2 = {
-    title: "Godzilla vs. Kong",
-    description: "Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted by Emperor Shang Tsung's best warrior, Sub-Zero",
-    date: "2021/05/05",
-    posterURI: "https://image.tmdb.org/t/p/w185/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg"
-}
-
-const Movies = () => (
-    <ScrollView style={{ paddingHorizontal: 20, paddingVertical: 22, backgroundColor: '#290F0F' }} >
-        <MovieCard style={{ marginBottom: 10 }} />
-        <MovieCard movieDetails={tempMov1} style={{ marginBottom: 10 }} />
-        <MovieCard movieDetails={tempMov2} style={{ marginBottom: 10 }} />
-        <MovieCard style={{ marginBottom: 10 }} />
-        <MovieCard movieDetails={tempMov1} style={{ marginBottom: 10 }} />
-        <MovieCard style={{ marginBottom: 10 }} />
-        <MovieCard movieDetails={tempMov2} style={{ marginBottom: 10 }} />
-        <MovieCard style={{ marginBottom: 10 }} />
-        <MovieCard movieDetails={tempMov1} style={{ marginBottom: 10 }} />
-        <MovieCard style={{ marginBottom: 10 }} />
-        <MovieCard movieDetails={tempMov2} style={{ marginBottom: 10 }} />
-        <MovieCard style={{ marginBottom: 10 }} />
-    </ScrollView >
-)
 
 export default Movies;
 
