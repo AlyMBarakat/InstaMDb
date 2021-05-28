27b0a085c45c156d86608a53d00cae5b4438ad4a
AlyMBarakat <aly.m.barakat@gmail.com>
2021-05-28 19:00:01 +0200

    Add loading skeleton

--- src/shared/components/MovieCard/MovieCard.js
+++ src/shared/components/MovieCard/MovieCard.js
@@ -1,26 +1,44 @@
+/**
+ * 
+ * 
+ * MovieCard Resusable component
+ * 
+ * 
+ */
 import React from 'react';
 import { View, Image } from 'react-native';
 import Text from '../Text';
 import styles from './MovieCard.styles';
 import ExpandableDescription from './ExpandableDescription';
+import MovieCardLoading from './MovieCard.loading';
 
-const MovieCard = React.memo(({ movieDetails, style }) => {
+const MovieCard = React.memo(({ loading, movieDetails, style }) => {
     return (
-        <View style={[style, styles.container]}>
-            <Image
-                source={{ uri: `https://image.tmdb.org/t/p/w342/${movieDetails.posterPath}` }}
-                style={styles.poster}
-            />
-            {/* Movie's data */}
-            <View style={styles.textContainer}>
-                {/* Title */}
-                <Text bold size={18} color='#D73B3B'>{movieDetails.title}</Text>
-                {/* Date */}
-                <Text size={13} color='#D88282' style={{ marginVertical: 3 }}>{movieDetails.releaseDate}</Text>
-                {/* ExpandableDescription */}
-                <ExpandableDescription text={movieDetails.overview} />
-            </View >
-        </View >
+        <>
+            {
+                loading ?
+                    // load card loading skeleton
+                    <MovieCardLoading />
+                    :
+                    // load movie card with data
+                    <View style={[style, styles.container]}>
+                        <Image
+                            source={{ uri: `https://image.tmdb.org/t/p/w342/${movieDetails.posterPath}` }}
+                            style={styles.poster}
+                        />
+                        {/* Movie's data */}
+                        <View style={styles.textContainer}>
+                            {/* Title */}
+                            <Text bold size={18} color='#D73B3B'>{movieDetails.title}</Text>
+                            {/* Date */}
+                            <Text size={13} color='#D88282' style={{ marginVertical: 3 }}>{movieDetails.releaseDate}</Text>
+                            {/* ExpandableDescription */}
+                            <ExpandableDescription text={movieDetails.overview} />
+                        </View >
+                    </View >
+
+            }
+        </>
     );
 });
 
